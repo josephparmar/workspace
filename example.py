4d202a361d8cb66730faebe0d16c245bd37b4752
joepar <joeparmar@outlook.com>
2026-08-14 12:33:07 +0100

    Inital commit

---
+++ example.py
@@ -0,0 +1,5 @@
+
+from jjp.functions import (
+   sort_dataframe 
+)
+
---
+++ jjp/functions.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def sort_dataframe(data : str, header : str):
+    '''
+    Converts the input csv to a pandas dataframe, 
+    converts all the data types to their auto types (str, int etc.),
+    and sorts the data by the user defined header
+
+    Args:
+        data (str or path): Path to input csv file, with headers. 
+        header (str or None): name of header you want to sort by. Use None for no sorting.
+
+    Returns:
+        df : sorted pandas dataframe. Turn column into numpy array by using df["header"].to_numpy()
+
+    '''
+    df = pd.read_csv(data)
+    df.convert_dtypes()
+    if header != None:
+        df.sort_values(by=[header], inplace=True, ignore_index=True)
+    return df 
